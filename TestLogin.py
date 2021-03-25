7a529d9e07219fdfeae33ada4f0acdc543960780
Akbol_Zhassakbayev <Akbol_Zhassakbayev@epam.com>
2021-03-25 13:57:14 +0600

    Task 5 was implemented

--- TestLogin.py
+++ TestLogin.py
@@ -1,3 +1,5 @@
+import time
+
 import pytest
 import softest
 from selenium import webdriver
@@ -6,20 +8,29 @@ from selenium.webdriver.support.wait import WebDriverWait
 
 
 class TestLogin(softest.TestCase):
+    driver = None
 
     @pytest.fixture(scope="class", autouse=True)
     def init_driver(self):
-        driver = webdriver.Chrome()
-        driver.wait = WebDriverWait(driver, 5)
+        self.driver = webdriver.Chrome()
+        self.driver.wait = WebDriverWait(self.driver, 5)
 
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument("--window-size=500x500")
 
-        driver.set_window_size(200, 100)
-        driver.get("https://tt-develop.quality-lab.ru")
-        driver.maximize_window()
+        self.driver.get("https://tt-develop.quality-lab.ru")
+        self.driver.set_window_size(200, 100)
+        self.driver.maximize_window()
         yield
-        driver.quit()
+        self.driver.quit()
 
     def test_incorrectUserNameAndPassword(self):
-        print("Hello TT")
+        name = self.driver.find_element_by_name("_username")
+        name.send_keys("TestUser")
+        password = self.driver.find_element_by_id("password")
+        password.send_keys("Password")
+        self.assertEqual(len(self.driver.find_elements_by_xpath("//*[text()='Invalid credentials.']")), 0)
+
+        submit = self.driver.find_element_by_xpath("//input[@value='Войти']")
+        submit.click()
+        self.assertEqual(len(self.driver.find_elements_by_xpath("//*[text()='Invalid credentials.']")), 1)
